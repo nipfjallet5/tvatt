800db9cf82e2e95c37864a8388f0e2a940749f79
Johan Lundqvist <jolundq@gmail.com>
2023-05-30 21:03:06 +0200

    wip

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# today=`date '+%Y-%m-%d'` -d '-1 day'
-today=`date '+%Y-%m-%d'`
-echo "${today}"
 
+starttime=`date '+%Y-%m-%dT%H:%M:%S' -d '-2 days'`
+endtime=`date '+%Y-%m-%dT%H:%M:%S' -d '-1 hour'`
+echo "${starttime}"
+echo "${endtime}"
 gh api repos/nipfjallet5/tvatt/actions/runs --paginate | \
-jq --arg v1 "${today}T00:00:00Z" --arg v2 "${today}T23:59:59Z" '.workflow_runs[] | select(.run_started_at > $v1) | select(.run_started_at < $v2)' | \
+jq --arg v1 "${starttime}Z" --arg v2 "${enddtime}Z" '.workflow_runs[] | select(.run_started_at > $v1) | select(.run_started_at < $v2)' | \
 jq '.id'
 
