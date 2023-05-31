758a8cb936141a2e523353d6b2bbadb1685bb314
Johan Lundqvist <jolundq@gmail.com>
2023-05-31 09:26:17 +0200

    wip

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@ endtime=`date '+%Y-%m-%dT%H:%M:%S' -d '-1 hour'`
 echo "${starttime}"
 echo "${endtime}"
 gh api repos/nipfjallet5/tvatt/actions/runs --paginate | \
-jq --arg v1 "${starttime}Z" --arg v2 "${enddtime}Z" '.workflow_runs[] | select(.run_started_at > $v1) | select(.run_started_at < $v2)' | \
+jq --arg v1 "${starttime}Z" --arg v2 "${enddtime}Z" '.workflow_runs[] | select(.run_started_at > $v1) | select(.run_started_at < $v2) | select(.display_title == "followup check")' | \
 jq '.id'
 
